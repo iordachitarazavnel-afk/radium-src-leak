54e58fe56719b1488a84520bd88b4dce8561bed2
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-01-22 18:01:57 +0200

    Update ChunkDataEvent.java

--- src/main/java/com/radium/client/events/event/ChunkDataEvent.java
+++ src/main/java/com/radium/client/events/event/ChunkDataEvent.java
@@ -1,8 +1,12 @@
 package com.radium.client.events.event;
 
+import com.radium.client.events.Event;
+import com.radium.client.events.Listener;
+
 import java.util.ArrayList;
 
 public class ChunkDataEvent extends Event<ChunkDataEvent.ChunkListener> {
+
     @Override
     public void fire(ArrayList<ChunkListener> listeners) {
         for (ChunkListener listener : listeners) listener.onChunkData(this);
