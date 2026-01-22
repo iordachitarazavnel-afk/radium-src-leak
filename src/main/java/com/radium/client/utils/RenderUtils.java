e6ba086f42f6648adebe1d25bbc9cf3a78f5f718
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-01-22 19:30:34 +0200

    Update RenderUtils.java

--- src/main/java/com/radium/client/utils/RenderUtils.java
+++ src/main/java/com/radium/client/utils/RenderUtils.java
@@ -1,22 +1,52 @@
 package com.radium.client.utils;
 
-import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.Camera;
+import com.mojang.blaze3d.systems.RenderSystem;
 
 public class RenderUtils {
 
-    // Desenează un cub
-    public static void drawBox(DrawContext context, int x1, int y1, int z1, int x2, int y2, int z2, int alpha) {
-        // Aici trebuie să adaugi codul tău OpenGL sau Minecraft Renderer pentru cub
-        // Placeholder:
-        // System.out.println("Drawing box from (" + x1+","+y1+","+z1+") to ("+x2+","+y2+","+z2+") with alpha " + alpha);
+    private static final MinecraftClient mc = MinecraftClient.getInstance();
+
+    /**
+     * Desenează un box în lumea 3D
+     * @param matrixStack MatrixStack de la Render3DEvent
+     * @param box Box-ul de desenat
+     * @param alpha Opacitatea (0-255)
+     */
+    public static void drawBox(MatrixStack matrixStack, Box box, int alpha) {
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+        RenderSystem.disableTexture();
+        RenderSystem.lineWidth(1.0F);
+        // Folosim WorldRenderer pentru a desena box-ul
+        WorldRenderer.drawBox(matrixStack, box, 1f, 0f, 0f, alpha / 255f); // roșu ca exemplu
+        RenderSystem.enableTexture();
+        RenderSystem.disableBlend();
     }
 
-    // Desenează un tracer
-    public static void drawTracer(DrawContext context, Vec3d from, Vec3d to, int alpha) {
-        // Placeholder pentru linia de tracer
-        // System.out.println("Drawing tracer from " + from + " to " + to + " alpha: " + alpha);
+    /**
+     * Desenează o linie (tracer) între două puncte
+     * @param matrixStack MatrixStack de la Render3DEvent
+     * @param from Start (de obicei player.getPos())
+     * @param to End (entitate)
+     * @param alpha Opacitate (0-255)
+     */
+    public static void drawTracer(MatrixStack matrixStack, Vec3d from, Vec3d to, int alpha) {
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+        RenderSystem.disableTexture();
+        RenderSystem.lineWidth(1.0F);
+        // Linie simplă între două puncte
+        WorldRenderer.drawLine(matrixStack, from, to, 0f, 1f, 0f, alpha / 255f); // verde ca exemplu
+        RenderSystem.enableTexture();
+        RenderSystem.disableBlend();
     }
 }
