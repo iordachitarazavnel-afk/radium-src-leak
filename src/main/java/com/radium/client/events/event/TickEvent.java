45daa68783466e306138d661e9074ed5d401fef6
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-01-22 19:09:41 +0200

    Update TickEvent.java

--- src/main/java/com/radium/client/events/event/TickEvent.java
+++ src/main/java/com/radium/client/events/event/TickEvent.java
@@ -1,6 +1,7 @@
 package com.radium.client.events.event;
 
-import  com.radium.client.event.CancellableEvent;
+import com.radium.client.events.event.CancellableEvent;
 
 public class TickEvent extends CancellableEvent {
 }
+
