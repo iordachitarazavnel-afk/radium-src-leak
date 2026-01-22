af260c399423336c5544148caafe158e98632821
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-01-22 15:01:45 +0200

    Update TickEvent.java

--- src/main/java/com/radium/client/events/event/TickEvent.java
+++ src/main/java/com/radium/client/events/event/TickEvent.java
@@ -1,5 +1,25 @@
-package com.radium.client.events.event;
+package com.radium.client.events.event.events;
 
-public class TickEvent {
-    // Poate fi extins ulterior dacă vrei info despre tick
+import com.radium.client.events.Event;
+import com.radium.client.events.Listener;
+
+import java.util.ArrayList;
+
+public class TickEvent extends Event<TickEvent.TickListener> {
+
+    public interface TickListener extends Listener {
+        void onTick();
+    }
+
+    @Override
+    public void fire(ArrayList<TickListener> listeners) {
+        for (TickListener listener : listeners) {
+            listener.onTick();
+        }
+    }
+
+    @Override
+    public Class<TickListener> getListenerType() {
+        return TickListener.class;
+    }
 }
