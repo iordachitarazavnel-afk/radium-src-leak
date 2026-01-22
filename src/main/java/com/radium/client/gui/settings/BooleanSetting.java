5adb98fd92f603498577a9b60d06eb78707604e2
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-01-22 16:46:48 +0200

    Update BooleanSetting.java

--- src/main/java/com/radium/client/gui/settings/BooleanSetting.java
+++ src/main/java/com/radium/client/gui/settings/BooleanSetting.java
@@ -1,18 +1,39 @@
-package com.radium.client.gui.setting;
+package com.radium.client.gui.settings;
 
-import com.radium.client.gui.settings;
+import com.radium.client.modules.Module;
 
-public class BooleanSetting extends Setting<Boolean> {
+public class BooleanSetting {
+
+    private final String name;
+    private boolean enabled;
+
+    private final Module parent;
 
     public BooleanSetting(String name, boolean defaultValue) {
-        super(name, defaultValue);
+        this.name = name;
+        this.enabled = defaultValue;
+        this.parent = null;
     }
 
-    public boolean getValue() {
-        return super.getValue();
+    public BooleanSetting(Module parent, String name, boolean defaultValue) {
+        this.name = name;
+        this.enabled = defaultValue;
+        this.parent = parent;
     }
 
-    public void setValue(boolean value) {
-        super.setValue(value);
+    public String getName() {
+        return name;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Module getParent() {
+        return parent;
     }
 }
