da97b3c459ef2bfd003c38e9785bc428ff3f0395
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-01-22 18:40:57 +0200

    Update EventListener.java

--- src/main/java/com/radium/client/events/EventListener.java
+++ src/main/java/com/radium/client/events/EventListener.java
@@ -2,9 +2,6 @@ package com.radium.client.events;
 
 import java.lang.annotation.*;
 
-/**
- * Annotare pentru metode care ascultă evenimente (Tick, Render3D, ChunkData etc.)
- */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface EventListener { }
