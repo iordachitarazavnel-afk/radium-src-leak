e355c47e6812bb1fb31c79179b468aaa62476c63
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-01-22 18:54:02 +0200

    Update SuspiciousESP.java

--- src/main/java/com/radium/client/modules/visual/SuspiciousESP.java
+++ src/main/java/com/radium/client/modules/visual/SuspiciousESP.java
@@ -5,59 +5,63 @@ import com.radium.client.gui.settings.BooleanSetting;
 import com.radium.client.gui.settings.NumberSetting;
 import com.radium.client.gui.settings.Setting;
 import com.radium.client.modules.Module;
-import com.radium.client.utils.EncryptedString;
 import com.radium.client.utils.RenderUtils;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.mob.PillagerEntity;
+import net.minecraft.entity.passive.LlamaEntity;
+import net.minecraft.entity.passive.MerchantEntity;
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.util.math.Box;
 import com.radium.client.events.event.EventListener;
 import com.radium.client.events.event.TickEvent;
 import com.radium.client.events.event.ChunkDataEvent;
 import com.radium.client.events.event.Render3DEvent;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.mob.WanderingTraderEntity;
-import net.minecraft.entity.passive.VillagerEntity;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Vec3d;
-
 import java.util.ArrayList;
 
 public class SuspiciousESP extends Module {
 
-    private final BooleanSetting wanderingTraders = new BooleanSetting(new EncryptedString("Wandering Traders"), true);
-    private final BooleanSetting villagers = new BooleanSetting(new EncryptedString("Villagers"), true);
-    private final NumberSetting alpha = new NumberSetting(new EncryptedString("Alpha"), 1, 255, 100, 1);
+    private final BooleanSetting wanderingTraders = new BooleanSetting("Wandering Traders", true);
+    private final BooleanSetting villagers = new BooleanSetting("Villagers", true);
+    private final BooleanSetting llamas = new BooleanSetting("Llamas", true);
+    private final BooleanSetting pillagers = new BooleanSetting("Pillagers", true);
+    private final BooleanSetting deepslate = new BooleanSetting("Deepslate", true);
+    private final BooleanSetting kelp = new BooleanSetting("Kelp", true);
+    private final NumberSetting deepslateMinY = new NumberSetting("Deepslate Min Y", -64, 320, 8, 1);
+    private final NumberSetting deepslateMaxY = new NumberSetting("Deepslate Max Y", -64, 320, 128, 1);
+    private final NumberSetting alpha = new NumberSetting("Alpha", 1, 255, 100, 1);
+    private final BooleanSetting tracers = new BooleanSetting("Tracers", false);
 
     public SuspiciousESP() {
-        super("SuspiciousESP", "Highlights certain entities in the world", Category.VISUAL);
-        addSettings(wanderingTraders, villagers, alpha);
+        super("SuspiciousESP", "Highlights entities like villagers, traders, and mobs", Category.VISUAL);
+        addSettings(wanderingTraders, villagers, llamas, pillagers, deepslate, kelp, deepslateMinY, deepslateMaxY, alpha, tracers);
     }
 
     @EventListener
     public void onTick(TickEvent event) {
-        // tick logic, optional
+        // aici poți adăuga logica de tick, dacă vrei să faci ceva periodic
     }
 
     @EventListener
     public void onChunkData(ChunkDataEvent event) {
-        // chunk data logic, optional
+        // aici poți adăuga logica când chunk-urile se încarcă
     }
 
     @EventListener
     public void onRender3D(Render3DEvent event) {
-        DrawContext context = event.getDrawContext(); // folosește DrawContext din Render3DEvent
         for (Entity entity : new ArrayList<>(MinecraftClient.getInstance().world.getEntities())) {
-            if ((wanderingTraders.getValue() && entity instanceof WanderingTraderEntity) ||
-                (villagers.getValue() && entity instanceof VillagerEntity)) {
+            if ((wanderingTraders.getValue() && entity instanceof MerchantEntity) ||
+                (villagers.getValue() && entity instanceof VillagerEntity) ||
+                (llamas.getValue() && entity instanceof LlamaEntity) ||
+                (pillagers.getValue() && entity instanceof PillagerEntity)) {
 
                 Box box = entity.getBoundingBox();
-                RenderUtils.drawBox(context, (int) box.minX, (int) box.minY, (int) box.minZ,
-                        (int) box.maxX, (int) box.maxY, (int) box.maxZ, alpha.getValue().intValue());
-
-                RenderUtils.drawTracer(context,
-                        MinecraftClient.getInstance().player.getPos(),
-                        entity.getPos(),
-                        alpha.getValue().intValue());
+                RenderUtils.drawBox(event.matrixStack, box, alpha.getValue().intValue());
+                if (tracers.getValue()) {
+                    RenderUtils.drawTracer(event.matrixStack, MinecraftClient.getInstance().player.getPos(), entity.getPos(), alpha.getValue().intValue());
+                }
             }
         }
     }
