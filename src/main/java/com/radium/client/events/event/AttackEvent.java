9be20e4e3db857e2c5d90ca0e2b176ae2897186a
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-01-22 19:23:06 +0200

    Update AttackEvent.java

--- src/main/java/com/radium/client/events/event/AttackEvent.java
+++ src/main/java/com/radium/client/events/event/AttackEvent.java
@@ -1,6 +1,25 @@
 package com.radium.client.events.event;
 
-import com.radium.client.events.event.CancellableEvent;
+import com.radium.client.events.CancellableEvent;
+import com.radium.client.events.Listener;
 
-public class AttackEvent extends CancellableEvent {
+import java.util.ArrayList;
+
+public class AttackEvent extends CancellableEvent<AttackEvent.AttackListener> {
+
+    @Override
+    public void fire(ArrayList<AttackListener> listeners) {
+        for (AttackListener l : listeners) {
+            l.onAttack(this);
+        }
+    }
+
+    @Override
+    public Class<AttackListener> getListenerType() {
+        return AttackListener.class;
+    }
+
+    public interface AttackListener extends Listener {
+        void onAttack(AttackEvent event);
+    }
 }
