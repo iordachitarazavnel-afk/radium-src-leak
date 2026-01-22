4748d1efcdcec4be2c3ab448581e7296a77dcc2a
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-01-22 19:12:20 +0200

    Update ChunkDataEvent.java

--- src/main/java/com/radium/client/events/event/ChunkDataEvent.java
+++ src/main/java/com/radium/client/events/event/ChunkDataEvent.java
@@ -1,12 +1,37 @@
 package com.radium.client.events.event;
 
+import com.radium.client.client.RadiumClient;
+import com.radium.client.events.CancellableEvent;
+import com.radium.client.events.Listener;
+import net.minecraft.network.packet.s2c.play.ChunkDataS2CPacket;
+import net.minecraft.world.chunk.WorldChunk;
+
 import java.util.ArrayList;
 
-public class ChunkDataEvent extends Event<ChunkDataEvent.ChunkListener> {
+public class ChunkDataEvent extends CancellableEvent<ChunkDataEvent.ChunkListener> {
+
+    private final ChunkDataS2CPacket packet;
+
+    public ChunkDataEvent(ChunkDataS2CPacket packet) {
+        this.packet = packet;
+    }
+
+    public ChunkDataS2CPacket getPacket() {
+        return packet;
+    }
+
+    public WorldChunk getChunk() {
+        if (RadiumClient.mc.world == null) return null;
+        return RadiumClient.mc.world.getChunk(packet.getChunkX(), packet.getChunkZ());
+    }
+
+    /* ================= EVENT SYSTEM ================= */
 
     @Override
     public void fire(ArrayList<ChunkListener> listeners) {
-        for (ChunkListener listener : listeners) listener.onChunkData(this);
+        for (ChunkListener listener : listeners) {
+            listener.onChunkData(this);
+        }
     }
 
     @Override
@@ -18,4 +43,3 @@ public class ChunkDataEvent extends Event<ChunkDataEvent.ChunkListener> {
         void onChunkData(ChunkDataEvent event);
     }
 }
-
