c6eacfe06ea43b6934f67038f58ca9cde7603f40
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-01-22 18:42:01 +0200

    Update Render3DEvent.java

--- src/main/java/com/radium/client/events/event/Render3DEvent.java
+++ src/main/java/com/radium/client/events/event/Render3DEvent.java
@@ -2,16 +2,20 @@ package com.radium.client.events.event;
 
 import com.radium.client.events.Event;
 import com.radium.client.events.Listener;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.gui.DrawContext;
 
 import java.util.ArrayList;
 
 public class Render3DEvent extends Event<Render3DEvent.Render3DListener> {
 
-    public final MatrixStack matrixStack;
+    private final DrawContext drawContext;
 
-    public Render3DEvent(MatrixStack matrixStack) {
-        this.matrixStack = matrixStack;
+    public Render3DEvent(DrawContext drawContext) {
+        this.drawContext = drawContext;
+    }
+
+    public DrawContext getDrawContext() {
+        return drawContext;
     }
 
     @Override
