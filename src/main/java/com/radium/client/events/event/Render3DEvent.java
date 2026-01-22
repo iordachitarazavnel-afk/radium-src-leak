395bd008d5e5ba18f41dad4362a4c5efa565289c
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-01-22 14:51:09 +0200

    Update Render3DEvent.java

---
+++ src/main/java/com/radium/client/events/event/Render3DEvent.java
@@ -0,0 +1,12 @@
+package com.radium.client.events.event.events;
+
+import net.minecraft.class_4587; // MatrixStack
+
+public class Render3DEvent {
+
+    public final class_4587 matrixStack;
+
+    public Render3DEvent(class_4587 matrixStack) {
+        this.matrixStack = matrixStack;
+    }
+}
