0d243c178093f2de62e4070abcdf4d075f3d80e9
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-01-22 17:16:28 +0200

    Update NumberSetting.java

--- src/main/java/com/radium/client/gui/settings/NumberSetting.java
+++ src/main/java/com/radium/client/gui/settings/NumberSetting.java
@@ -1,25 +1,38 @@
 package com.radium.client.gui.settings;
 
 public class NumberSetting extends Setting<Double> {
+
+    private double value;
     private final double min;
     private final double max;
     private final double increment;
 
     public NumberSetting(String name, double defaultValue, double min, double max, double increment) {
-        super(name, defaultValue);
+        super(name);
+        this.value = defaultValue;
         this.min = min;
         this.max = max;
         this.increment = increment;
     }
 
-    public double getValue() {
+    @Override
+    public Double getValue() {
         return value;
     }
 
-    public void setValue(double value) {
+    @Override
+    public void setValue(Double value) {
         if (value < min) value = min;
         if (value > max) value = max;
         this.value = value;
+    }
+
+    public double getDoubleValue() {
+        return value;
+    }
+
+    public void setDoubleValue(double value) {
+        setValue(value);
     }
 
     public double getMin() {
