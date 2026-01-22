d25eb4e53863b5dfd523504b8658d4a5bc0c0d92
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-01-22 19:08:13 +0200

    Update AttackEvent.java

--- src/main/java/com/radium/client/events/event/AttackEvent.java
+++ src/main/java/com/radium/client/events/event/AttackEvent.java
@@ -1,37 +1,6 @@
 package com.radium.client.events.event;
 
-import com.radium.client.events.Event;
-import com.radium.client.events.Listener;
+import com.radium.client.events.event.CancellableEvent;
 
-import net.minecraft.entity.Entity;
-
-import java.util.ArrayList;
-
-public class AttackEvent extends Event<AttackEvent.AttackListener> {
-
-    private final Entity target;
-
-    public AttackEvent(Entity target) {
-        this.target = target;
-    }
-
-    public Entity getTarget() {
-        return target;
-    }
-
-    public interface AttackListener extends Listener {
-        void onAttack(AttackEvent event);
-    }
-
-    @Override
-    public void fire(ArrayList<AttackListener> listeners) {
-        for (AttackListener listener : listeners) {
-            listener.onAttack(this);
-        }
-    }
-
-    @Override
-    public Class<AttackListener> getListenerType() {
-        return AttackListener.class;
-    }
+public class AttackEvent extends CancellableEvent {
 }
