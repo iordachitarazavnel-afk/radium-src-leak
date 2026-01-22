38ca1eb9f5ddbb0fc063afd1c4db5c4ce79529e8
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-01-22 19:29:04 +0200

    Update Render3DEvent.java

--- src/main/java/com/radium/client/events/event/Render3DEvent.java
+++ src/main/java/com/radium/client/events/event/Render3DEvent.java
@@ -1,24 +1,20 @@
 package com.radium.client.events.event;
 
-import com.radium.client.events.Event;
-import com.radium.client.events.Listener;
-
+import com.radium.client.events.CancellableEvent;
 import net.minecraft.client.util.math.MatrixStack;
 
-import java.util.ArrayList;
+public class Render3DEvent extends CancellableEvent<Render3DEvent.Render3DListener> {
 
-public class Render3DEvent extends Event<Render3DEvent.Render3DListener> {
+    public final MatrixStack matrixStack;
 
-    public final MatrixStack matrices;
-
-    public Render3DEvent(MatrixStack matrices) {
-        this.matrices = matrices;
+    public Render3DEvent(MatrixStack matrixStack) {
+        this.matrixStack = matrixStack;
     }
 
     @Override
-    public void fire(ArrayList<Render3DListener> listeners) {
-        for (Render3DListener l : listeners) {
-            l.onRender3D(this);
+    public void fire(java.util.ArrayList<Render3DListener> listeners) {
+        for (Render3DListener listener : listeners) {
+            listener.onRender3D(this);
         }
     }
 
@@ -27,7 +23,7 @@ public class Render3DEvent extends Event<Render3DEvent.Render3DListener> {
         return Render3DListener.class;
     }
 
-    public interface Render3DListener extends Listener {
+    public interface Render3DListener extends com.radium.client.events.Listener {
         void onRender3D(Render3DEvent event);
     }
 }
