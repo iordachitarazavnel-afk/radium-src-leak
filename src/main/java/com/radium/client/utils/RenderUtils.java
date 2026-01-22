b92d3bb2fdc8f856ea233b98358e46e8e245726e
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-01-22 18:42:25 +0200

    Update RenderUtils.java

--- src/main/java/com/radium/client/utils/RenderUtils.java
+++ src/main/java/com/radium/client/utils/RenderUtils.java
@@ -1,45 +1,22 @@
 package com.radium.client.utils;
 
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.client.render.Camera;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.client.MinecraftClient;
 
-import java.awt.Color;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
+public class RenderUtils {
 
-public final class RenderUtils {
-
-    private RenderUtils() {} // prevent instantiation
-
-    public static Camera getCamera() {
-        return net.minecraft.client.MinecraftClient.getInstance().gameRenderer.getCamera();
+    // Desenează un cub
+    public static void drawBox(DrawContext context, int x1, int y1, int z1, int x2, int y2, int z2, int alpha) {
+        // Aici trebuie să adaugi codul tău OpenGL sau Minecraft Renderer pentru cub
+        // Placeholder:
+        // System.out.println("Drawing box from (" + x1+","+y1+","+z1+") to ("+x2+","+y2+","+z2+") with alpha " + alpha);
     }
 
-    public static BlockPos getCameraPos() {
-        return new BlockPos(net.minecraft.client.MinecraftClient.getInstance().player.getX(),
-                            net.minecraft.client.MinecraftClient.getInstance().player.getEyeY(),
-                            net.minecraft.client.MinecraftClient.getInstance().player.getZ());
-    }
-
-    public static void renderFilledBox(MatrixStack matrices,
-                                       float x1, float y1, float z1,
-                                       float x2, float y2, float z2,
-                                       Color color) {
-        // Aici va fi codul tău OpenGL sau WorldRenderer pentru cub
-    }
-
-    public static void renderBlockCache(MatrixStack matrices,
-                                        ConcurrentHashMap<Long, Set<BlockPos>> blockCache,
-                                        Color color) {
-        for (Set<BlockPos> blocks : blockCache.values()) {
-            for (BlockPos pos : blocks) {
-                renderFilledBox(matrices, pos.getX(), pos.getY(), pos.getZ(),
-                                pos.getX() + 1f, pos.getY() + 1f, pos.getZ() + 1f, color);
-            }
-        }
+    // Desenează un tracer
+    public static void drawTracer(DrawContext context, Vec3d from, Vec3d to, int alpha) {
+        // Placeholder pentru linia de tracer
+        // System.out.println("Drawing tracer from " + from + " to " + to + " alpha: " + alpha);
     }
 }
