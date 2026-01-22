8679d004c61f3877eba14177da3143a63a5fe984
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-01-22 19:35:04 +0200

    Update RenderUtils.java

--- src/main/java/com/radium/client/utils/RenderUtils.java
+++ src/main/java/com/radium/client/utils/RenderUtils.java
@@ -1,52 +1,45 @@
 package com.radium.client.utils;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.Camera;
-import com.mojang.blaze3d.systems.RenderSystem;
 
-public class RenderUtils {
+public class RenderUtil {
 
     private static final MinecraftClient mc = MinecraftClient.getInstance();
 
     /**
-     * Desenează un box în lumea 3D
-     * @param matrixStack MatrixStack de la Render3DEvent
-     * @param box Box-ul de desenat
-     * @param alpha Opacitatea (0-255)
+     * Desenează un box în lume (wireframe)
      */
-    public static void drawBox(MatrixStack matrixStack, Box box, int alpha) {
-        RenderSystem.enableBlend();
-        RenderSystem.defaultBlendFunc();
-        RenderSystem.disableTexture();
-        RenderSystem.lineWidth(1.0F);
-        // Folosim WorldRenderer pentru a desena box-ul
-        WorldRenderer.drawBox(matrixStack, box, 1f, 0f, 0f, alpha / 255f); // roșu ca exemplu
-        RenderSystem.enableTexture();
-        RenderSystem.disableBlend();
+    public static void drawBox(MatrixStack matrixStack, Box box, int r, int g, int b, int alpha) {
+        VertexConsumerProvider.Immediate provider = mc.getBufferBuilders().getEntityVertexConsumers();
+        VertexConsumer vertexConsumer = provider.getBuffer(RenderLayer.getLines());
+
+        WorldRenderer.drawBox(matrixStack, vertexConsumer,
+                box.minX, box.minY, box.minZ,
+                box.maxX, box.maxY, box.maxZ,
+                r / 255f, g / 255f, b / 255f, alpha / 255f);
+
+        provider.draw();
     }
 
     /**
      * Desenează o linie (tracer) între două puncte
-     * @param matrixStack MatrixStack de la Render3DEvent
-     * @param from Start (de obicei player.getPos())
-     * @param to End (entitate)
-     * @param alpha Opacitate (0-255)
      */
-    public static void drawTracer(MatrixStack matrixStack, Vec3d from, Vec3d to, int alpha) {
-        RenderSystem.enableBlend();
-        RenderSystem.defaultBlendFunc();
-        RenderSystem.disableTexture();
-        RenderSystem.lineWidth(1.0F);
-        // Linie simplă între două puncte
-        WorldRenderer.drawLine(matrixStack, from, to, 0f, 1f, 0f, alpha / 255f); // verde ca exemplu
-        RenderSystem.enableTexture();
-        RenderSystem.disableBlend();
+    public static void drawTracer(MatrixStack matrixStack, Vec3d from, Vec3d to, int r, int g, int b, int alpha) {
+        VertexConsumerProvider.Immediate provider = mc.getBufferBuilders().getEntityVertexConsumers();
+        VertexConsumer vertexConsumer = provider.getBuffer(RenderLayer.getLines());
+
+        WorldRenderer.drawLine(matrixStack, vertexConsumer,
+                from.x, from.y, from.z,
+                to.x, to.y, to.z,
+                r / 255f, g / 255f, b / 255f, alpha / 255f);
+
+        provider.draw();
     }
 }
