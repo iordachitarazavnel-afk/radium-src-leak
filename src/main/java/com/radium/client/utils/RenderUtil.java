7411ea2c6b17c3da09ccceb66a5dfe38281353b4
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-01-23 17:24:26 +0200

    Update RenderUtil.java

--- src/main/java/com/radium/client/utils/RenderUtil.java
+++ src/main/java/com/radium/client/utils/RenderUtil.java
@@ -1,19 +1,18 @@
 package com.radium.client.utils;
 
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.Box;
-import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.util.math.Vec3d;
 
 public class RenderUtil {
 
-    public static void drawBox(MatrixStack matrixStack, Box box, float alpha) {
-        // placeholder simplu
-        WorldRenderer.drawBox(matrixStack, null, box.minX, box.minY, box.minZ, box.maxX, box.maxY, box.maxZ, 1f, 0f, 0f, alpha);
+    public static void drawBox(MatrixStack matrices, Box box, float r, float g, float b, float alpha) {
+        // aici trebuie să folosești WorldRenderer sau metodele tale de render
+        // exemplu minimal
     }
 
-    public static void drawTracer(MatrixStack matrixStack, net.minecraft.util.math.Vec3d from, net.minecraft.util.math.Vec3d to, float alpha) {
-        // placeholder simplu
-        WorldRenderer.drawLine(matrixStack, from, to, 0f, 1f, 0f, alpha);
+    public static void drawTracer(MatrixStack matrices, Vec3d from, Vec3d to, float alpha) {
+        // exemplu minimal, folosește liniile OpenGL sau metoda ta de tracer
     }
 }
