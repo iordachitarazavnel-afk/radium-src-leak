e7a3c79a9158190adb4d1a333e7e5d35436b8752
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-01-23 17:12:43 +0200

    Update Render3DEvent.java

--- src/main/java/com/radium/client/events/event/Render3DEvent.java
+++ src/main/java/com/radium/client/events/event/Render3DEvent.java
@@ -2,17 +2,14 @@ package com.radium.client.events.event;
 
 import com.radium.client.events.CancellableEvent;
 import net.minecraft.client.util.math.MatrixStack;
-import org.joml.Matrix4f;
 
 public class Render3DEvent extends CancellableEvent {
 
     private final MatrixStack matrixStack;
-    private final Matrix4f matrix4f;
     private final float tickDelta;
 
-    public Render3DEvent(MatrixStack matrixStack, Matrix4f matrix4f, float tickDelta) {
+    public Render3DEvent(MatrixStack matrixStack, float tickDelta) {
         this.matrixStack = matrixStack;
-        this.matrix4f = matrix4f;
         this.tickDelta = tickDelta;
     }
 
