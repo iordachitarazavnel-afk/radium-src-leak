224ab78df942dfde7697c630893273cbf730b39c
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-01-23 17:35:36 +0200

    Update EnchantmentUtil.java

--- src/main/java/com/radium/client/utils/EnchantmentUtil.java
+++ src/main/java/com/radium/client/utils/EnchantmentUtil.java
@@ -4,19 +4,13 @@ import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.ItemStack;
 
-import java.util.Map;
-
 public class EnchantmentUtil {
 
     public static boolean hasEnchantment(ItemStack stack, Enchantment enchantment) {
-        return EnchantmentHelper.getEnchantments(stack).containsKey(enchantment);
+        return EnchantmentHelper.getLevel(enchantment, stack) > 0;
     }
 
-    public static int getLevel(ItemStack stack, Enchantment enchantment) {
+    public static int getEnchantmentLevel(ItemStack stack, Enchantment enchantment) {
         return EnchantmentHelper.getLevel(enchantment, stack);
-    }
-
-    public static Map<Enchantment, Integer> getEnchantments(ItemStack stack) {
-        return EnchantmentHelper.getEnchantments(stack);
     }
 }
