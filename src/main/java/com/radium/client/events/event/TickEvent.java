fb9c6bfea6b7eabf4e09286437f3b3d2034c75be
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-01-23 17:02:35 +0200

    Update TickEvent.java

--- src/main/java/com/radium/client/events/event/TickEvent.java
+++ src/main/java/com/radium/client/events/event/TickEvent.java
@@ -2,21 +2,5 @@ package com.radium.client.events.event;
 
 import com.radium.client.events.CancellableEvent;
 
-public class TickEvent extends CancellableEvent<TickEvent.TickListener> {
-
-    @Override
-    public void fire(java.util.ArrayList<TickListener> listeners) {
-        for (TickListener listener : listeners) {
-            listener.onTick(this);
-        }
-    }
-
-    @Override
-    public Class<TickListener> getListenerType() {
-        return TickListener.class;
-    }
-
-    public interface TickListener extends com.radium.client.events.Listener {
-        void onTick(TickEvent event);
-    }
+public class TickEvent extends CancellableEvent {
 }
