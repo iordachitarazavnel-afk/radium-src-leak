3abda33ffb37085dd0d4f6d2de92ac60b60b4567
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-01-23 17:38:21 +0200

    Update EventHook.java

--- src/main/java/com/radium/client/events/event/EventHook.java
+++ src/main/java/com/radium/client/events/event/EventHook.java
@@ -1,17 +1,36 @@
 package com.radium.client.events.event;
 
-import com.radium.client.client.RadiumClient; // folosește clientul tău
+import com.radium.client.RadiumClient;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.network.ClientPlayerEntity;
 
+/**
+ * EventHook se ocupă cu trimiterea evenimentelor din Minecraft către EventBus-ul RadiumClient.
+ * În acest exemplu, trimite un AttackEvent când jucătorul atacă o entitate.
+ */
 public class EventHook {
 
+    /**
+     * Apelat când playerul atacă o entitate.
+     * @param target entitatea atacată
+     */
     public static void onAttack(PlayerEntity target) {
-        AttackEvent attackEvent = new AttackEvent(); // fără parametri
-        RadiumClient.EVENT_BUS.fire(attackEvent); // folosește EventManager-ul din RadiumClient
+        // Creăm un eveniment AttackEvent
+        AttackEvent attackEvent = new AttackEvent();
 
-        if (!attackEvent.isCancelled()) {
-            // codul real de atac aici
+        // Poți adăuga aici orice informație dacă vrei să extinzi AttackEvent
+        // ex: attackEvent.target = target;
+
+        // Trimitem evenimentul către EventBus-ul RadiumClient
+        RadiumClient.EVENT_BUS.fire(attackEvent);
+
+        // Dacă evenimentul a fost anulat, împiedică atacul efectiv
+        if (attackEvent.isCancelled()) {
+            // Blocăm atacul: depinde de contextul tău cum faci asta
+            ClientPlayerEntity player = RadiumClient.mc.player;
+            if (player != null) {
+                player.swingHand(player.getActiveHand()); // exemplu simplu
+            }
         }
     }
 }
-
